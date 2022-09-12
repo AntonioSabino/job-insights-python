807468a4815d12dd30a9df8e663353522abb95b1
Antonio Sabino <aslimajr@outlook.com>
2022-09-12 20:22:49 -0300

    feat: implentação de testes para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.counter import count_ocurrences
+import pytest
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count_javascript = count_ocurrences("src/jobs.csv", "javaScript")
+    assert count_javascript == 122
+
+    count_python = count_ocurrences("src/jobs.csv", "PYTHON")
+    assert count_python == 1639
+
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("src/jobs.cs", "javaScript")
