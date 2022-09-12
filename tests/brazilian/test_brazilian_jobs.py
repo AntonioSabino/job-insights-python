ca4c740efc488f96a5b00d174d11c54de7ad5f2d
Antonio Sabino <aslimajr@outlook.com>
2022-09-12 20:41:27 -0300

    feat: implementação dos testes para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,16 @@
-# from src.brazilian_jobs import read_brazilian_file
+import pytest
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs_list = read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv"
+    )
+
+    for job in brazilian_jobs_list:
+        assert "title" in job.keys()
+        assert "salary" in job.keys()
+        assert "type" in job.keys()
+
+    with pytest.raises(FileNotFoundError):
+        read_brazilian_file("tests/mocks/argentinians_jobs")
